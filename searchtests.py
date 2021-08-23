36b8e179413761d565f8749871dbfb41dee582d1
Alexander_Acosta <alexanderacosta@unach.cl>
2021-08-22 21:56:53 -0400

    Abortar curso

--- searchtests.py
+++ searchtests.py
@@ -6,9 +6,26 @@ class HomePageTests(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path = './chromedriver')
         driver = self.driver
-        driver.get('http://www.demo-store.seleniumacademy.com/')
+        driver.implicitly_wait(30)
         driver.maximize_window()
-        driver.implicitly_wait(15)
+        driver.get('http://www.demo-store.seleniumacademy.com/')
+
+    def test_search_tee(self):
+        driver = self.driver
+        search_field = driver.find_element_by_id("q")
+        search_field.clear()
+        search_field.send_keys('tee')
+        search_field.submit()
+
+    def test_search_salt_shaker(self):
+        driver = self.driver
+        search_field = driver.find_element_by_name("q")
+
+        search_field.send_keys('salt shaker')
+        search_field.submit()
+
+        products = driver.find_elements_by_xpath("h2.product-name")
+        self.assertEqual(1, len(products))
 
     def test_search_text_field(self):
         search_field = self.driver.find_element_by_id("search")
@@ -19,8 +36,22 @@ class HomePageTests(unittest.TestCase):
     def test_search_text_field_class_name(self):
         search_field = self.driver.find_element_by_class_name("input-text")
 
+    def test_search_button_enable(self):
+        button = self.driver.find_element_by_class_name("button")
+
+    def test_count_of_promo_banner_images(self):
+        banner_list = self.driver.find_elements_by_class_name("promos")
+        banners = banner_list.find_elements_by_tag_name("img")
+        self.assertEqual(3, len(banners))
+
+    def test_vip_promo(self):
+        vip_promo = self.driver.find_element_by_xpath("//img[@alt='VIP']")
+
+    def test_shopping_cart(self):
+        shopping_cart_icon = self.driver.find_element_by_css_selector("div.header-minicart span.icon")
+
     def tearDown(self):
         self.driver.quit()
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main(verbosity = 2)
